f7944aa1ec5d6469bc021933efed76864affbbec
devagx <alungroome@hotmail.com>
2021-01-31 12:23:07 +0000

    Implemented exercise 005 - rewrote a subset of functions in exercise 4 to use implicit return

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -11,29 +11,44 @@ function findNamesBeginningWith(names, char) {
   return names.filter(x => x.charAt(0).toUpperCase() === char);
 }
 
+/*
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
   return words.filter(x => x.substring(0, 3).toUpperCase() == "TO ");
 }
+*/
+const findVerbs = w => w.filter(x => x.substring(0, 3).toUpperCase() == "TO ");
 
+/*
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   return nums.filter(x => Number.isInteger(x));
 }
+*/
 
+const getIntegers = n => n.filter(x => Number.isInteger(x));
+
+/*
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
   return users.map(x => x.data.city.displayName);
 }
+*/
 
+const getCities = u => u.map(x => x.data.city.displayName)
+
+/*
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   return nums.map(x => Number(Math.sqrt(x).toFixed(2)));
 }
+*/
+
+const getSquareRoots = n => n.map(x => Number(Math.sqrt(x).toFixed(2)));
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
